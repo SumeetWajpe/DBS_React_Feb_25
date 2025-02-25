44e7e5ade3ab90170cbabe035a644fc86e282027
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-02-25 09:50:58 +0530

    ✨ Creating dynamic message component instances

--- Day 1/UsingCRA/my-first-app/src/App.js
+++ Day 1/UsingCRA/my-first-app/src/App.js
@@ -2,12 +2,48 @@ import "./App.css";
 import React from "react";
 import Message from "./Message";
 class App extends React.Component {
+  messages = [
+    {
+      msg: "Hello",
+      from: "John",
+      to: "James",
+      imageUrl:
+        "https://media.istockphoto.com/id/1496192609/vector/hello-written-speech-bubble-on-yellow-background.jpg?s=612x612&w=0&k=20&c=WGz6PfIO8lVjj3FkgDYZYMpfXFFyIjTIm_mg1aQc-lA=",
+    },
+    {
+      msg: "Hey",
+      from: "John",
+      to: "James",
+      imageUrl:
+        "https://media.istockphoto.com/id/1496192609/vector/hello-written-speech-bubble-on-yellow-background.jpg?s=612x612&w=0&k=20&c=WGz6PfIO8lVjj3FkgDYZYMpfXFFyIjTIm_mg1aQc-lA=",
+    },
+    {
+      msg: "Bye",
+      from: "John",
+      to: "James",
+      imageUrl:
+        "https://media.istockphoto.com/id/1496192609/vector/hello-written-speech-bubble-on-yellow-background.jpg?s=612x612&w=0&k=20&c=WGz6PfIO8lVjj3FkgDYZYMpfXFFyIjTIm_mg1aQc-lA=",
+    },
+  ];
   render() {
     return (
       <div>
-        <Message msg="Hello" />
-        <Message msg="Hi" />
-        <Message msg="Bye" />
+        {this.messages.map(m => (
+          <Message messagedetails={m} />
+        ))}
+
+        {/* <Message
+          msg="Hi"
+          from="James"
+          to="Bond"
+          imageUrl="https://media.istockphoto.com/id/1496192609/vector/hello-written-speech-bubble-on-yellow-background.jpg?s=612x612&w=0&k=20&c=WGz6PfIO8lVjj3FkgDYZYMpfXFFyIjTIm_mg1aQc-lA="
+        />
+        <Message
+          msg="Bye"
+          from="Peter"
+          to="Parker"
+          imageUrl="https://media.istockphoto.com/id/1496192609/vector/hello-written-speech-bubble-on-yellow-background.jpg?s=612x612&w=0&k=20&c=WGz6PfIO8lVjj3FkgDYZYMpfXFFyIjTIm_mg1aQc-lA="
+        /> */}
       </div>
     );
   }
--- Day 1/UsingCRA/my-first-app/src/Message.jsx
+++ Day 1/UsingCRA/my-first-app/src/Message.jsx
@@ -2,6 +2,18 @@ import React from "react";
 
 export default class Message extends React.Component {
   render() {
-    return <h2> {this.props.msg} </h2>;
+    return (
+      <div>
+        <img
+          src={this.props.messagedetails.imageUrl}
+          alt={this.props.messagedetails.msg}
+          height="200"
+          width="200"
+        />
+        <h2> {this.props.messagedetails.msg} </h2>
+        <p> From : {this.props.messagedetails.from}</p>
+        <p> To : {this.props.messagedetails.to}</p>
+      </div>
+    );
   }
 }
