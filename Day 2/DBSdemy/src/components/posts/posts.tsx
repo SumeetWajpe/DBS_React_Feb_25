15937c4724156a87696745c0f5f984c86feb0ef1
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-02-26 12:50:24 +0530

    ✨ using useEffect with Post title dispalyed

--- Day 2/DBSdemy/src/components/posts/posts.tsx
+++ Day 2/DBSdemy/src/components/posts/posts.tsx
@@ -1,24 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Posts: React.FC = () => {
+  const [posts, setPosts] = useState<any>([]);
   useEffect(() => {
     axios
-      .get("", {})
+      .get("http://localhost:3500/posts")
       .then(function (response) {
         // handle success
-        console.log(response.data);
+        // console.log(response.data);
+        setPosts(response.data);
       })
       .catch(function (error) {
         // handle error
         console.log(error);
       });
-  });
+    console.log("Done !");
+  }, []);
   return (
     <div>
       <header>
         <h1>All Posts</h1>
       </header>
+      <main>
+        <ul>
+          {posts.map((p: any) => (
+            <li key={p.id}>{p.title}</li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 };
