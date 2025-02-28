8820e341145e78de1a42b46ea718b192bf1b2d26
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-02-28 14:46:13 +0530

    ✨ Webpack & Babel

---
+++ Day 5/WebpackAndBabel/src/Calc.js
@@ -0,0 +1,6 @@
+import * as MathModule from "./Math.js";
+console.log("The addition is : " + MathModule.Add(30, 40));
+console.log("The product is : " + MathModule.Product(30, 40));
+
+var point = new MathModule.Point();
+console.log(point.x);
---
+++ Day 5/WebpackAndBabel/src/Math.js
@@ -0,0 +1,13 @@
+export class Point {
+  constructor() {
+    this.x = 100;
+    this.y = 100;
+  }
+}
+export var Add = (x, y) => {
+  return x + y;
+};
+
+export function Product(x, y) {
+  return x * y;
+}
